cd374a79ae0b0db89a231d6d4395d2022c89931a
David H <github@hnd.mn>
2022-01-16 23:43:25 +0000

    Add DELETE endpoint.

--- src/routes/todos/index.json.ts
+++ src/routes/todos/index.json.ts
@@ -1,27 +1,14 @@
 import type { RequestHandler } from "@sveltejs/kit";
-import { text } from "svelte/internal";
+import { api } from "./_api";
 
-// TODO: Persist in database
-let todos: Todo[] = [];
-
-export const get: RequestHandler = () => {
-    return {
-        status: 200,
-        body: todos
-    }
+export const get: RequestHandler = (request) => {
+  return api(request);
 }
 
 export const post: RequestHandler<{}, FormData> = (request) => {
-    todos.push({
-        created_at: new Date(),
-        text: request.body.get("text"),
-        done: false
-    });
-    
-    return {
-        status: 303,
-        headers: {
-            location: "/"
-        }
-    }
+  return api(request, {
+    created_at: new Date(),
+    text: request.body.get("text"),
+    done: false
+  });
 }
